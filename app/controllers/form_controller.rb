e7e54f902d0c8c1049728b3892acec8c7bbbcc73
Edy <edigleyssonsilva@gmail.com>
2024-03-30 17:38:06 -0300

    play a bit with form fields

--- app/controllers/form_controller.rb
+++ app/controllers/form_controller.rb
@@ -1,4 +1,17 @@
 class FormController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  def show
+    render(body: 'show form')
+  end
+
+  def create
+    puts "received a request"
+
+    redirect_to(action: 'show', id: 100)
+  end
 end
