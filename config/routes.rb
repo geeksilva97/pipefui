239140d332ff80180917ec14689119477fcbad4d
Edy <edigleyssonsilva@gmail.com>
2024-04-02 22:51:34 -0300

    routes workaraound

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,12 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :dashboard do
-    collection do
-      resources :form do
-        post :add_field, on: :collection
-        post :remove_field, on: :collection
-        # get :answer
-        # get 'answer/:id', to: 'form#full_answer'
-      end
+  scope :dashboard do
+    resources :form do
+      post :add_field, on: :collection
+      post :remove_field, on: :collection
+      get :answer, as: 'dashboard_answer'
+      get 'answer/:id', to: 'form#full_answer', as: 'full_answer'
     end
   end
 
